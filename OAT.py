16d172e5e47f5dc078581fc5b70602f9478fb50c
lovesoo <lovesoo@qq.com>
2017-07-24 16:03:04 +0800

    用例裁剪逻辑优化，新增参数mode,num

--- OAT.py
+++ OAT.py
@@ -12,56 +12,58 @@ from itertools import groupby
 from collections import OrderedDict
 import os
 
+
 def dataSplit(data):
-    ds=[]
-    mb=[sum([k for m,k in data['mk'] if m<=10]),sum([k for m,k in data['mk'] if m>10])]
+    ds = []
+    mb = [sum([k for m, k in data['mk'] if m <= 10]), sum([k for m, k in data['mk'] if m > 10])]
     for i in data['data']:
-        if mb[1]==0:
+        if mb[1] == 0:
             ds.append([int(d) for d in i])
-        elif mb[0]==0:
-            ds.append([int(i[n*2:(n+1)*2]) for n in range(mb[1])])
+        elif mb[0] == 0:
+            ds.append([int(i[n * 2:(n + 1) * 2]) for n in range(mb[1])])
         else:
-            part_1=[int(j) for j in i[:mb[0]]]
-            part_2=[int(i[mb[0]:][n*2:(n+1)*2]) for n in range(mb[1])]
-            ds.append(part_1+part_2)
+            part_1 = [int(j) for j in i[:mb[0]]]
+            part_2 = [int(i[mb[0]:][n * 2:(n + 1) * 2]) for n in range(mb[1])]
+            ds.append(part_1 + part_2)
     return ds
 
-class OAT( object ):
-    def __init__(self,OAFile=os.path.split(os.path.realpath(__file__))[0]+'/data/ts723_Designs.txt' ):
+
+class OAT(object):
+    def __init__(self, OAFile=os.path.split(os.path.realpath(__file__))[0] + '/data/ts723_Designs.txt'):
         """
         初始化解析构造正交表对象，数据来源：http://support.sas.com/techsup/technote/ts723_Designs.txt
         """
-        self.data={}
+        self.data = {}
 
-        #解析正交表文件数据
-        with open( OAFile, ) as f:
+        # 解析正交表文件数据
+        with open(OAFile, ) as f:
             # 定义临时变量
             key = ''
-            value = [ ]
+            value = []
             pos = 0
 
             for i in f:
                 i = i.strip()
                 if 'n=' in i:
                     if key and value:
-                        self.data[ key ] = dict( pos=pos,
-                                                 n=int( key.split( 'n=' )[ 1 ].strip() ),
-                                                 mk=[ [int(mk.split( '^' )[0]),int(mk.split( '^' )[1])] for mk in key.split( 'n=' )[ 0 ].strip().split( ' ' ) ],
-                                                 data=value )
-                    key = ' '.join( [ k for k in i.split( ' ' ) if k ] )
-                    value = [ ]
+                        self.data[key] = dict(pos=pos,
+                                              n=int(key.split('n=')[1].strip()),
+                                              mk=[[int(mk.split('^')[0]), int(mk.split('^')[1])] for mk in key.split('n=')[0].strip().split(' ')],
+                                              data=value)
+                    key = ' '.join([k for k in i.split(' ') if k])
+                    value = []
                     pos += 1
                 elif i:
-                    value.append( i )
+                    value.append(i)
 
-            self.data[ key ] = dict( pos=pos,
-                                     n=int( key.split( 'n=' )[ 1 ].strip() ),
-                                     mk=[[int(mk.split( '^' )[0]),int(mk.split( '^' )[1])]for mk in key.split( 'n=' )[ 0 ].strip().split( ' ' ) ],
-                                     data=value )
-        self.data = sorted( self.data.items(), key=lambda i: i[ 1 ][ 'pos' ] )
+            self.data[key] = dict(pos=pos,
+                                  n=int(key.split('n=')[1].strip()),
+                                  mk=[[int(mk.split('^')[0]), int(mk.split('^')[1])]for mk in key.split('n=')[0].strip().split(' ')],
+                                  data=value)
+        self.data = sorted(self.data.items(), key=lambda i: i[1]['pos'])
 
     @staticmethod
-    def get(self,mk):
+    def get(self, mk):
         """
         传入参数：mk列表，如[(2,3)],[(5,5),(2,1)]
 
@@ -73,41 +75,46 @@ class OAT( object ):
        2. 查询正交表
         这里简单处理，只返回满足>=m,n,k条件的n最小数据，未做复杂的数组包含校验
         """
-        mk=sorted(mk,key=lambda i:i[0])
+        mk = sorted(mk, key=lambda i: i[0])
 
-        m = max( [ i[ 0 ] for i in mk ] )
-        k = sum( [ i[ 1 ] for i in mk ] )
-        n = sum( [ i[ 1 ] * (i[ 0 ] - 1) for i in mk ] ) + 1
-        query_key = ' '.join( [ '^'.join( [ str( j ) for j in i ] ) for i in mk ] )
+        m = max([i[0] for i in mk])
+        k = sum([i[1] for i in mk])
+        n = sum([i[1] * (i[0] - 1) for i in mk]) + 1
+        query_key = ' '.join(['^'.join([str(j) for j in i]) for i in mk])
 
         for data in self.data:
             # 先查询是否有完全匹配的正交表数据
             if query_key in data[0]:
                 return dataSplit(data[1])
             # 否则返回满足>=m,n,k条件的n最小数据
-            elif data[1]['n']>=n and data[1]['mk'][0][0]>=m and data[1]['mk'][0][1]>=k:
+            elif data[1]['n'] >= n and data[1]['mk'][0][0] >= m and data[1]['mk'][0][1] >= k:
                 return dataSplit(data[1])
-        #无结果
+        # 无结果
         return None
 
-    def genSets(self,params):
+    def genSets(self, params, mode=0, num=1):
         """
         传入测试参数OrderedDict，调用正交表生成测试集
+        mode:用例裁剪模式，取值0,1
+            0 宽松模式，只裁剪重复测试集
+            1 严格模式，除裁剪重复测试集外，还裁剪含None测试集(num为允许None测试集最大数目)
         """
-        sets=[]
-        mk=[(k,len(list(v)))for k,v in groupby(params.items(),key=lambda x:len(x[1]))]
-        data=OAT.get(self,mk)
+        sets = []
+        mk = [(k, len(list(v)))for k, v in groupby(params.items(), key=lambda x:len(x[1]))]
+        data = OAT.get(self, mk)
         for d in data:
-            #根据正则表结果生成测试集
-            q=OrderedDict()
+            # 根据正则表结果生成测试集
+            q = OrderedDict()
             for index, (k, v) in zip(d, params.items()):
                 try:
-                    q[k]=v[index]
+                    q[k] = v[index]
                 except IndexError:
-                    #参数取值超出范围时，取None
-                    q[k]=None
-            if q not in sets and any(map(lambda v: v is not None,q.values())):
-                # 测试集裁剪,去除重复及全None测试集
-                sets.append(q)
+                    # 参数取值超出范围时，取None
+                    q[k] = None
+            if q not in sets:
+                if mode == 0:
+                    sets.append(q)
+                elif mode == 1 and (len(filter(lambda v: v is None, q.values()))) <= num:
+                    # 测试集裁剪,去除重复及含None测试集
+                    sets.append(q)
         return sets
-
--- test.py
+++ test.py
@@ -12,23 +12,32 @@ from OAT import *
 import json
 
 if __name__ == "__main__":
-    oat = OAT ()
-    case1 = OrderedDict ( [ ('K1', [ 0, 1 ]),
-                            ('K2', [ 0, 1 ]),
-                            ('K3', [ 0, 1 ]) ] )
+    oat = OAT()
+    case1 = OrderedDict([('K1', [0, 1]),
+                         ('K2', [0, 1]),
+                         ('K3', [0, 1])])
 
-    case2 = OrderedDict ( [ ('A', [ 'A1', 'A2', 'A3' ]),
-                            ('B', [ 'B1', 'B2', 'B3', 'B4' ]),
-                            ('C', [ 'C1', 'C2', 'C3' ]),
-                            ('D', [ 'D1', 'D2']) ] )
+    case2 = OrderedDict([('A', ['A1', 'A2', 'A3']),
+                         ('B', ['B1', 'B2', 'B3', 'B4']),
+                         ('C', ['C1', 'C2', 'C3']),
+                         ('D', ['D1', 'D2'])])
 
-    case3 = OrderedDict ( [ (u'对比度', [ u'正常', u'极低', u'低', u'高', u'极高' ]),
-                            (u'色彩效果', [ u'无', u'黑白', u'棕褐色', u'负片', u'水绿色' ]),
-                            (u'感光度', [ u'自动', 100, 200, 400, 800 ]),
-                            (u'白平衡', [ u'自动', u'白炽光', u'日光', u'荧光', u'阴光' ]),
-                            (u'照片大小', [ '5M', '3M', '2M', '1M', 'VGA' ]),
-                            (u'闪光模式', [ u'开', u'关' ]) ] )
+    case3 = OrderedDict([(u'对比度', [u'正常', u'极低', u'低', u'高', u'极高']),
+                         (u'色彩效果', [u'无', u'黑白', u'棕褐色', u'负片', u'水绿色']),
+                         (u'感光度', [u'自动', 100, 200, 400, 800]),
+                         (u'白平衡', [u'自动', u'白炽光', u'日光', u'荧光', u'阴光']),
+                         (u'照片大小', ['5M', '3M', '2M', '1M', 'VGA']),
+                         (u'闪光模式', [u'开', u'关'])])
+
+    case4 = OrderedDict([('A', ['A1', 'A2', 'A3', 'A4', 'A5', 'A6']),
+                         ('B', ['B1']),
+                         ('C', ['C1'])])
 
     print json.dumps(oat.genSets(case1))
     print json.dumps(oat.genSets(case2))
     print json.dumps(oat.genSets(case3))
+    print json.dumps(oat.genSets(case4))
+    print json.dumps(oat.genSets(case4, 1, 0))
+    print json.dumps(oat.genSets(case4, 1, 1))
+    print json.dumps(oat.genSets(case4, 1, 2))
+    print json.dumps(oat.genSets(case4, 1, 3))
